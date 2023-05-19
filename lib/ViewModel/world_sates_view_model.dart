a9fbf53cab9bb39ca4983bf3715f6db23befe39d
iabdulwahab7 <abdulwahabmk446@gmail.com>
2023-05-19 15:34:46 +0500

    first commit

---
+++ lib/ViewModel/world_sates_view_model.dart
@@ -0,0 +1,34 @@
+
+import 'dart:convert';
+import 'package:covid19_tracker/Model/world_states.dart';
+import 'package:covid19_tracker/ViewModel/Utilities/app_url.dart';
+import 'package:http/http.dart' as http;
+
+class WorldStatesViewModel {
+
+
+  Future<WorldStatesModel> fetchWorldRecords() async {
+    final response = await http.get(Uri.parse(AppUrl.worldStatesApi));
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      return WorldStatesModel.fromJson(data);
+    } else {
+      throw Exception('Error');
+    }
+  }
+
+  Future<List<dynamic>> countriesListApi() async {
+    var data ;
+    final response = await http.get(Uri.parse(AppUrl.countriesList));
+    print(response.statusCode.toString());
+    print(data);
+    if (response.statusCode == 200) {
+       data = jsonDecode(response.body.toString());
+      return data;
+    } else {
+      throw Exception('Error');
+    }
+  }
+
+}
